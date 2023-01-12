eef469dfdff0f3d5728dad7815e40d18e1619b17
Vickkk <justusofnigeria@gmail.com>
2023-01-12 11:37:14 +0100

    Input layer with gsap works well

--- js/eachMovie.js
+++ js/eachMovie.js
@@ -1,3 +1,11 @@
+const timeline = gsap.timeline();
+
+const cancelButton = document.querySelector(".search-layer button");
+const searchButton = document.querySelector(".nav-theme .git-hub button");
+const searchLayer = document.querySelector(".search-layer");
+cancelButton.addEventListener("click", cancelSearchLayer);
+searchButton.addEventListener("click", callSearchLayer);
+
 var swiper = new Swiper(".swiper", {
 	effect: "cards",
 	grabCursor: true,
@@ -6,6 +14,7 @@ var swiper = new Swiper(".swiper", {
 // const searchParams = new URLSearchParams();
 // const getParams = searchParams.get();
 
+// add skeleton loader
 function addSkeletonLoader() {
 	const skeletonItem = document.getElementById("movies-recommendation");
 	const parent = document.querySelector(".recommendations .grid-wrapper");
@@ -15,3 +24,45 @@ function addSkeletonLoader() {
 	}
 }
 addSkeletonLoader();
+
+// toggle search button
+function cancelSearchLayer(e) {
+	e.stopPropagation();
+	if (searchLayer.classList.contains("active")) {
+		gsap.to(searchLayer, { display: "none", xPercent: "100", yPercent: "-100", ease: "linear", scale: 0 });
+		searchLayer.classList.remove("active");
+	}
+}
+
+function callSearchLayer(e) {
+	e.stopPropagation();
+	if (!searchLayer.classList.contains("active")) {
+		searchLayer.classList.add("active");
+		const formtitle = document.querySelector(".search-layer-inner h2");
+		const formInput = document.querySelector(".search-layer-inner form .form-floating");
+		timeline
+			.from(searchLayer, {
+				xPercent: "100",
+				yPercent: "-100",
+			})
+			.to(
+				searchLayer,
+				{
+					xPercent: "0",
+					yPercent: "0",
+					scale: 1,
+					display: "block",
+				},
+				"<"
+			)
+			.from(
+				[formtitle, formInput],
+				{
+					y: 40,
+					opacity: 0,
+					stagger: 0.25,
+				},
+				"<0.45"
+			);
+	}
+}
