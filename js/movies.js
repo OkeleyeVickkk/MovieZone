726a88d4ee005a89b1eab447e40e7ecc32bd061a
Vickkk <justusofnigeria@gmail.com>
2023-01-17 09:40:38 +0100

    read more functionality added

--- js/movies.js
+++ js/movies.js
@@ -11,11 +11,12 @@ const cancelButton = document.querySelector(".search-layer .cancel-button button
 const submitButton = document.querySelector(".search-layer-inner [type='submit']");
 const searchButton = document.querySelector(".nav-theme .git-hub button");
 const searchLayer = document.querySelector(".search-layer");
+const readMoreButton = document.querySelector(".read_more button");
 
 cancelButton.addEventListener("click", cancelSearchLayer);
 searchButton.addEventListener("click", callSearchLayer);
+readMoreButton.addEventListener("click", readMore);
 
-// toggle search button
 function cancelSearchLayer(e) {
 	e.stopPropagation();
 	if (searchLayer.classList.contains("active")) {
@@ -55,21 +56,56 @@ function callSearchLayer(e) {
 	}
 }
 
-const searchMovie = async () => {
-	const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US`;
+let defaultPageNumber = 1;
+
+function readMore(e) {
+	e.stopPropagation();
+
+	// const skeletonItem = document.getElementById("skeleton-loader");
+	// const parent = document.querySelector(".movie-wrapper");
+	// for (let i = 0; i < 20; i++) {
+	// 	const cloneSkeletons = skeletonItem.content.cloneNode(true);
+	// 	parent.innerHTML += cloneSkeletons;
+	// }
+	const num = ++defaultPageNumber;
+	searchMovie(num);
+}
+
+const searchMovie = async (num) => {
+	const parent = document.querySelector(".movie-wrapper");
+	const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${num}`;
 	const response = await fetch(url);
 	if (!response.ok) throw " Error occured! ";
-	const skeletonItem = document.getElementById("skeleton-loader");
-	const parent = document.querySelector(".movie-wrapper");
 	const data = await response.json();
-	for (let i = 0; i < data.results.length; i++) {
-		const cloneSkeletons = skeletonItem.content.cloneNode(true);
-		parent.appendChild(cloneSkeletons);
+
+	const movieTemplate = document.getElementById("movie-item");
+	const movies = data.results;
+	movies.forEach((movie) => {
+		console.log(movie);
+		const clone = movieTemplate.content.cloneNode(true);
+
+		const { backdrop_path, poster_path, title, vote_average, id } = movie;
+
+		clone.querySelector(".movie-image-wrapper img").src = `${image_base_url}${backdrop_path ?? poster_path}`;
+		clone.querySelector(".movie-content .movie-title").textContent = title;
+		clone.querySelector(".movie-image-wrapper .vote-average .value").textContent = filterVoteAverage(vote_average);
+
+		parent.append(clone); //paste to the screen
+	});
+
+	function filterVoteAverage(vote_average) {
+		let [rateMajor, rateMinor] = vote_average.toString().split(".");
+		console.log(rateMajor, rateMinor);
+		if (rateMinor) {
+			rateMinor.length > 1 ? rateMinor.split("")[0] : rateMinor;
+		} else {
+			rateMinor = (0).toString();
+		}
+		return `${parseInt(rateMajor)}.${parseInt(rateMinor)}`;
 	}
-	console.log(data);
 };
 
 searchMovie();
 // read more function
-// searc function
+// search function
 // intersect observer
